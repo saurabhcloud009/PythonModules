5b8a761a83b5aa11218e476e13a45a4929310d37
Saurabh <saurabhpathaks21@gmail.com>
2019-02-20 16:12:12 +0530

    Update cleanall.py

--- cleanall.py
+++ cleanall.py
@@ -86,6 +86,9 @@ for region in regions:
         print ("About to delete %s | in %s" % (volume['VolumeId'], region['RegionName']))
         response = client.delete_volume(VolumeId=volume['VolumeId'])
         
+################################Global Services#################################
+
+
 ################################ S3 Buckets ####################################
 print ("+++++++++++++ Starting S3 now -----------------") 
 client = boto3.client('s3')
